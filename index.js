1edef822ed84e69fe0b190fed3860a0a7c50d8d3
Ham Vocke <hermann.vocke@gmail.com>
2017-11-11 20:32:21 +0100

    setup jest for testing

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log("started")
 let name = 'Ham';
 let time = 'today';
 console.log('Hello ' + name + ', how are you ' + time + '?');
+
+export function sum(a, b) {
+    return a+b;
+}
+
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import { sum } from './index';
+
+test('adds two numbers', () => {
+    expect(sum(1,2)).toBe(3);
+})
