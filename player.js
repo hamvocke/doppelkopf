6234bff33cf4ad1f2b31a68413fa83288e64b56a
Ham Vocke <hermann.vocke@gmail.com>
2017-11-18 18:20:49 +0100

    players can play cards

--- player.js
+++ player.js
@@ -1,6 +1,18 @@
+import { find, pull } from 'lodash';
+
 export default class Player {
     constructor(name) {
         this.name = name;
         this.hand = [];
+    }
+
+    play(card) {
+        let cardToBePlayed = find(this.hand, card);
+
+        if(!cardToBePlayed) {
+            throw 'can\'t play a card that\'s not on the player\'s hand';
+        }
+
+        pull(this.hand, cardToBePlayed);
     }
 }
--- player.test.js
+++ player.test.js
@@ -1,11 +1,38 @@
 import Player from './player';
+import { king, queen, suites } from './card';
+
 
 test('player has a name', () => {
     const player = new Player("Ham");
     expect(player.name).toBe("Ham");
-})
+});
 
-test('player has an empty hand', () => {
+test('new player has an empty hand', () => {
     const player = new Player("Ham");
     expect(player.hand).toHaveLength(0);
-})
+});
+
+test('player can play card from hand', () => {
+    const player = new Player("Ham");
+    const kingOnHand = king.of(suites.diamonds);
+    const queenOnHand = queen.of(suites.spades);
+    player.hand = [kingOnHand, queenOnHand];
+
+    player.play(king.of(suites.diamonds));
+
+    expect(player.hand).not.toContain(kingOnHand);
+    expect(player.hand).toContain(queenOnHand);
+});
+
+test('player cannot play card that is not on their hand', () => {
+    const player = new Player('Ham');
+    player.hand = [
+        king.of(suites.diamonds)
+    ];
+
+    function invalidMove() {
+        player.play(queen.of(suites.diamonds));
+    }
+
+    expect(invalidMove).toThrowError('can\'t play a card that\'s not on the player\'s hand');
+});
