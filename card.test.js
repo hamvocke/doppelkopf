f5ca0861a57a318e5f440fb664bf7f8ca237211d
Ham Vocke <hermann.vocke@gmail.com>
2017-11-18 17:52:15 +0100

    use suites object instead of string literals

--- card.test.js
+++ card.test.js
@@ -1,27 +1,27 @@
 import Card from './card';
-import {ace, ten, king, queen, jack} from './card';
+import {ace, ten, king, queen, jack, suites} from './card';
 
 test('ace has a value of 11', () => {
-    const aceOfClubs = ace.of('clubs');
+    const aceOfClubs = ace.of(suites.clubs);
     expect(aceOfClubs.value).toBe(11);
 })
 
 test('ten has a value of 10', () => {
-    const tenOfDiamonds = ten.of('diamonds');
+    const tenOfDiamonds = ten.of(suites.diamonds);
     expect(tenOfDiamonds.value).toBe(10);
 })
 
 test('king has a value of 4', () => {
-    const kingOfHearts = king.of('hearts');
+    const kingOfHearts = king.of(suites.hearts);
     expect(kingOfHearts.value).toBe(4);
 })
 
 test('queen has a value of 3', () => {
-    const queenOfPikes = queen.of('pikes');
+    const queenOfPikes = queen.of(suites.pikes);
     expect(queenOfPikes.value).toBe(3);
 })
 
 test('jack has a value of 2', () => {
-    const jackOfClubs = jack.of('clubs');
+    const jackOfClubs = jack.of(suites.clubs);
     expect(jackOfClubs.value).toBe(2);
 })
