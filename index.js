14f4d416101375f9e643069e9877b92113ebe9e5
Ham Vocke <hermann.vocke@gmail.com>
2017-11-19 12:04:38 +0100

    debug game configuration in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import Game from './game';
 
+const game = new Game();
+
+console.log('New game started');
+
+console.log('Players:\n');
+game.players.forEach(player => logPlayer(player));
+
+function logPlayer(player) {
+    console.log('Player', player.name);
+    console.log('Cards on hand:');
+    player.hand.cards.forEach(card => console.log('\t' + card.rank + ' of ' + card.suite));
+}
