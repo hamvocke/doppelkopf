985c779bf030b05163d1e7135a262e98b243f077
Ham Vocke <hermann.vocke@gmail.com>
2017-12-12 21:47:55 +0100

    fix eslint errors in hand model

--- src/models/hand.js
+++ src/models/hand.js
@@ -1,38 +1,38 @@
-import { queen, suites, compare } from './card';
-import { find, pull } from 'lodash';
+import { queen, suites, compare } from '@/models/card'
+import { find, pull } from 'lodash'
 
 export default class Hand {
-    constructor(cards = []) {
-        this.cards = cards;
+  constructor (cards = []) {
+    this.cards = cards
+  }
+
+  isRe () {
+    return find(this.cards, queen.of(suites.clubs))
+  }
+
+  isKontra () {
+    return !this.isRe()
+  }
+
+  value () {
+    return this.cards
+        .map(card => card.value)
+        .reduce((acc, value) => acc + value, 0)
+  }
+
+  find (card) {
+    return find(this.cards, card)
+  }
+
+  remove (card) {
+    if (!this.find(card)) {
+      throw 'can\'t remove card that isn\'t on hand'
     }
 
-    isRe() {
-        return find(this.cards, queen.of(suites.clubs));
-    }
+    pull(this.cards, card)
+  }
 
-    isKontra() {
-        return !this.isRe();
-    }
-
-    value() {
-        return this.cards
-            .map(card => card.value)
-            .reduce((acc, value) => acc + value, 0);
-    }
-
-    find(card) {
-        return find(this.cards, card);
-    }
-
-    remove(card) {
-        if(!this.find(card)) {
-            throw 'can\'t remove card that isn\'t on hand';
-        }
-
-        pull(this.cards, card);
-    }
-
-    sort() {
-        this.cards.sort(compare).reverse();
-    }
+  sort () {
+    this.cards.sort(compare).reverse()
+  }
 }
