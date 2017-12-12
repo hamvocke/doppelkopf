00618e798351b332a132636b461c4412b1df4f06
Ham Vocke <hermann.vocke@gmail.com>
2017-12-12 21:44:51 +0100

    fix eslint errors in deck model

--- src/models/deck.js
+++ src/models/deck.js
@@ -1,24 +1,23 @@
-import { Card } from './card';
-import { ranks, suites } from './card';
-import { shuffle, flatten } from 'lodash';
+import { Card, values, ranks, suites } from '@/models/card'
+import { shuffle, flatten } from 'lodash'
 
 export class Deck {
-    constructor() {
-        this.cards = shuffle([]
-            .concat(...allCards)
-            .concat(...allCards));
-    }
+  constructor () {
+    this.cards = shuffle([]
+        .concat(...allCards)
+        .concat(...allCards))
+  }
 
-    get value() {
-        return values[this.rank];
-    }
+  get value () {
+    return values[this.rank]
+  }
 }
 
 export const allCards = flatten(Object.values(ranks).map(rank => {
-    return [
-        new Card(suites.clubs, rank),
-        new Card(suites.spades, rank),
-        new Card(suites.hearts, rank),
-        new Card(suites.diamonds, rank),
-    ];
-}));
+  return [
+    new Card(suites.clubs, rank),
+    new Card(suites.spades, rank),
+    new Card(suites.hearts, rank),
+    new Card(suites.diamonds, rank)
+  ]
+}))
