6a7891e22cb291296ecae2a6c04896293c585342
Ham Vocke <hermann.vocke@gmail.com>
2017-12-12 21:48:46 +0100

    fix eslint errors in player.js

--- src/models/player.js
+++ src/models/player.js
@@ -1,20 +1,20 @@
-import Hand from './hand';
+import Hand from '@/models/hand'
 
 export default class Player {
-    constructor(name, game) {
-        this.name = name;
-        this.hand = new Hand();
-        this.game = game;
+  constructor (name, game) {
+    this.name = name
+    this.hand = new Hand()
+    this.game = game
+  }
+
+  play (card) {
+    let cardToBePlayed = this.hand.find(card)
+
+    if (!cardToBePlayed) {
+      throw 'can\'t play a card that\'s not on the player\'s hand'
     }
 
-    play(card) {
-        let cardToBePlayed = this.hand.find(card);
-
-        if(!cardToBePlayed) {
-            throw 'can\'t play a card that\'s not on the player\'s hand';
-        }
-
-        this.game.currentTrick.add(cardToBePlayed);
-        this.hand.remove(cardToBePlayed);
-    }
+    this.game.currentTrick.add(cardToBePlayed)
+    this.hand.remove(cardToBePlayed)
+  }
 }
