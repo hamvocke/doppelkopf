09964f2ea9fdf213f5cce7dea041e18b5506a733
Ham Vocke <hermann.vocke@gmail.com>
2018-01-21 21:30:56 +0100

    bind docker container to port 80

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 ssh root@ham.codes 'docker pull hamvocke/doppelkopf'
 ssh root@ham.codes 'docker stop doppelkopf'
 ssh root@ham.codes 'docker rm doppelkopf'
-ssh root@ham.codes 'docker run -it -d -p 80:8080 --name doppelkopf hamvocke/doppelkopf'
+ssh root@ham.codes 'docker run -it -d -p 80:80 --name doppelkopf hamvocke/doppelkopf'
