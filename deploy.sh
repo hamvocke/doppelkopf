9f99e99b9492e532a0db5034854154049a2bf405
Ham Vocke <hermann.vocke@gmail.com>
2018-03-18 19:13:19 +0100

    rotate ssh key, change deployment to dokku

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh root@ham.codes 'docker pull hamvocke/doppelkopf'
-ssh root@ham.codes 'docker stop doppelkopf'
-ssh root@ham.codes 'docker rm doppelkopf'
-ssh root@ham.codes 'docker run -it -d -p 80:80 --name doppelkopf hamvocke/doppelkopf'
+ssh root@ham.codes 'docker pull hamvocke/doppelkopf:latest'
+ssh root@ham.codes 'docker tag hamvocke/doppelkopf dokku/doppelkopf:latest'
+ssh root@ham.codes 'dokku tags:deploy doppelkopf'
+
