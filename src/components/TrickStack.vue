3be1a9b765325557ebf190df097aea7e12a0e619
Ham Vocke <hermann.vocke@gmail.com>
2018-04-22 20:50:55 +0200

    display number of won tricks

--- src/components/TrickStack.vue
+++ src/components/TrickStack.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="trickStack">
-    <Card v-for='card in this.trickStack.cards()' :is-covered='true' :key='card.cardId' :card='card' />
+  <div class="trickStack" v-if='this.trickStack.tricks.length > 0'>
+    <p class="trickCount">Stiche: {{ this.trickStack.tricks.length }}</p>
+    <Card :is-covered='true' />
+    <Card :is-covered='true' />
+    <Card :is-covered='true' />
+    <Card :is-covered='true' />
   </div>
 </template>
 
@@ -21,5 +25,16 @@ export default {
 </script>
 
 <style scoped>
+@import "assets/css/colors.css";
+
+.trickStack {
+  margin: 6px;
+  padding: 6px;
+  display: inline-block;
+}
+
+.card {
+  margin-left: -60px;
+}
 
 </style>
--- test/unit/specs/components/TrickStack.test.js
+++ test/unit/specs/components/TrickStack.test.js
@@ -12,11 +12,26 @@ trick.add(ace.of(suits.hearts), player1)
 trick.add(queen.of(suits.spades), player2)
 
 describe('TrickStack.vue', () => {
-  test('should render each card', () => {
+  test('should render nothing if player has no trick', () => {
+    const emptyTrickStack = new TrickStackModel()
+    const wrapper = mount(TrickStack, { propsData: { trickStack: emptyTrickStack } })
+    expect(wrapper.find('div.trickStack').exists()).toBe(false)
+  })
+
+  test('should render one trick as soon as player has a trick', () => {
     const trickStack = new TrickStackModel()
     trickStack.add(trick)
     const wrapper = mount(TrickStack, { propsData: { trickStack: trickStack } })
     expect(trick.playedCards).toHaveLength(2)
-    expect(wrapper.findAll('div.card')).toHaveLength(2)
+    expect(wrapper.find('div.trickStack').exists()).toBe(true)
+    expect(wrapper.findAll('div.card')).toHaveLength(4)
+  })
+
+  test('should display number of won tricks', () => {
+    const trickStack = new TrickStackModel()
+    trickStack.add(trick)
+    trickStack.add(trick)
+    const wrapper = mount(TrickStack, { propsData: { trickStack: trickStack } })
+    expect(wrapper.find('p.trickCount').text()).toContain('2')
   })
 })
