7ac7a69f6ed7e0f8557ff290aa8976792a7b0b84
Ham Vocke <hermann.vocke@gmail.com>
2018-04-28 15:42:37 +0200

    calculate scores

--- src/models/scorecard.js
+++ src/models/scorecard.js
@@ -1,5 +1,17 @@
 export class Scorecard {
   constructor (players) {
     this.players = players
+    this.scoreLines = []
+  }
+
+  addScoreLine (scoreLine) {
+    this.scoreLines.push(scoreLine)
+  }
+
+  scoreFor (player) {
+    return this.scoreLines.reduce((acc, scoreLine) => {
+      acc += scoreLine[player.name]
+      return acc
+    }, 0)
   }
 }
--- test/unit/specs/models/scorecard.test.js
+++ test/unit/specs/models/scorecard.test.js
@@ -2,10 +2,39 @@ import { Scorecard } from '@/models/scorecard'
 import { Player } from '@/models/player'
 
 describe('Scorecard', () => {
-  test('should play contain all players', () => {
-    const players = [new Player('one'), new Player('two'), new Player('three')]
-    const scorecard = new Scorecard(players)
+  const players = [new Player('one'), new Player('two'), new Player('three'), new Player('four')]
+  const scorecard = new Scorecard(players)
 
-    expect(scorecard.players).toHaveLength(3)
+  test('should play contain all players', () => {
+    expect(scorecard.players).toHaveLength(4)
+  })
+
+  test('should initialize all players with 0 points', () => {
+    expect(scorecard.scoreFor(players[0])).toBe(0)
+    expect(scorecard.scoreFor(players[1])).toBe(0)
+    expect(scorecard.scoreFor(players[2])).toBe(0)
+    expect(scorecard.scoreFor(players[3])).toBe(0)
+  })
+
+  test('should calculate scores', () => {
+    const firstScore = {}
+    firstScore[players[0].name] = 4
+    firstScore[players[1].name] = -4
+    firstScore[players[2].name] = -4
+    firstScore[players[3].name] = 4
+
+    const secondScore = {}
+    secondScore[players[0].name] = -2
+    secondScore[players[1].name] = 2
+    secondScore[players[2].name] = -2
+    secondScore[players[3].name] = 2
+
+    scorecard.addScoreLine(firstScore)
+    scorecard.addScoreLine(secondScore)
+
+    expect(scorecard.scoreFor(players[0])).toBe(2)
+    expect(scorecard.scoreFor(players[1])).toBe(-2)
+    expect(scorecard.scoreFor(players[2])).toBe(-6)
+    expect(scorecard.scoreFor(players[3])).toBe(6)
   })
 })
