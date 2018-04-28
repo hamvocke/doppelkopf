013a938aee415a17526cbc835a792a8964f34557
Ham Vocke <hermann.vocke@gmail.com>
2018-04-28 15:54:23 +0200

    create scorecard component

---
+++ src/components/Scorecard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="scorecard" >
+    <div class="players">
+      <span class="player" v-for='player in scorecard.players'>{{ player.name }}</span>
+    </div>
+    <div class="score" v-for='scoreLine in scorecard.scoreLines'>
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scorecard',
+  props: {
+    scorecard: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.notifications {
+  grid-area: notifications;
+}
+
+.message {
+  text-align: center;
+  margin: 0px auto 6px;
+  width: 100%;
+  padding: 6px;
+  background: rgba(255, 255, 255, 0.3);
+  color: #333;
+}
+
+</style>
---
+++ test/unit/specs/components/Scorecard.test.js
@@ -0,0 +1,12 @@
+import Scorecard from '@/components/Scorecard'
+import { Scorecard as ScorecardModel } from '@/models/scorecard'
+import { mount } from '@vue/test-utils'
+
+describe('Scorecard.vue', () => {
+  it('should display scorecard', () => {
+    const model = new ScorecardModel()
+    const wrapper = mount(Scorecard, {propsData: { scorecard: model }})
+
+    expect(wrapper.find('.players').exists()).toBe(true)
+  })
+})
