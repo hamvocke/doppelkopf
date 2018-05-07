1d28e2d5174b1b1af18c90ed52285ed953903237
Ham Vocke <hermann.vocke@gmail.com>
2018-05-07 22:10:34 +0200

    cleanup deployment

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ssh root@ham.codes 'docker pull hamvocke/doppelkopf:latest'
-ssh root@ham.codes 'docker tag hamvocke/doppelkopf dokku/doppelkopf:latest'
-ssh root@ham.codes 'dokku tags:deploy doppelkopf'
-
