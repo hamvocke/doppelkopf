a5d03f70764a0fc4c4a9c7d7e12def9ccd0af67b
Ham Vocke <hermann.vocke@gmail.com>
2018-07-01 19:42:26 +0200

    add sentry error tracking

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 
 Vue.config.productionTip = false
 
@@ -11,3 +13,8 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+Raven
+    .config('https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644')
+    .addPlugin(RavenVue, Vue)
+    .install()
