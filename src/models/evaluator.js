ba1f60863b8cf52d8bcd914b764305c5c766135f
Ham Vocke <hermann.vocke@gmail.com>
2018-09-17 21:47:27 +0200

    add trick evaluation to trick evaluator

--- src/models/evaluator.js
+++ src/models/evaluator.js
@@ -1,5 +1,12 @@
+import { find } from 'lodash'
+
 export class Evaluator {
   constructor (game = {}) {
     this.game = game
+  }
+
+  evaluateTrick (trick) {
+    const winner = find(this.game.players, { name: trick.winner() })
+    winner.win(trick)
   }
 }
--- test/unit/specs/models/evaluator.test.js
+++ test/unit/specs/models/evaluator.test.js
@@ -1,9 +1,14 @@
 import { Evaluator } from '@/models/evaluator'
 import { Game } from '@/models/game'
+import { Trick } from '@/models/trick'
+import { ace, jack, suits } from '@/models/card'
 
 let evaluator
+let players
+
 beforeEach(() => {
   evaluator = new Game().evaluator
+  players = evaluator.game.players
 })
 
 describe('Evaluator', () => {
@@ -13,5 +18,17 @@ describe('Evaluator', () => {
 
   test('should know the game', () => {
     expect(evaluator.game).toBeDefined()
+  })
+
+  test('should give trick to winner', () => {
+    const trick = new Trick(4)
+    trick.add(ace.of(suits.hearts), players[0])
+    trick.add(jack.of(suits.diamonds), players[1])
+    trick.add(ace.of(suits.hearts), players[2])
+    trick.add(jack.of(suits.hearts), players[3])
+
+    evaluator.evaluateTrick(trick)
+
+    expect(players[3].trickStack.tricks).toHaveLength(1)
   })
 })
