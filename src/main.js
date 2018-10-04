9824787727be5433e811dcd4ee5aadf30042a85a
Ham Vocke <hermann.vocke@gmail.com>
2018-10-04 20:10:41 +0200

    upgrade to vue-cli 3

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import Raven from 'raven-js'
-import RavenVue from 'raven-js/plugins/vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import Raven from "raven-js";
+import RavenVue from "raven-js/plugins/vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+}).$mount("#app");
 
 Raven
-    .config('https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644')
-    .addPlugin(RavenVue, Vue)
-    .install()
+     .config('https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644')
+     .addPlugin(RavenVue, Vue)
+     .install()
+
