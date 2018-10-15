5d5d7b02bf231057cd15aaffa846c741031a8164
Ham Vocke <hermann.vocke@gmail.com>
2018-10-15 19:44:15 +0200

    outline frontend deployment script

--- ci/deploy_frontend.sh
+++ ci/deploy_frontend.sh
@@ -1,7 +1,26 @@
 #! /usr/bin/env bash
 
 source _colors.sh
+set -e
 
-step "Some step"
-error "This is an error"
-success "Success!"
+step "Deploying frontend"
+
+step "Upload static content..."
+#rsync -rvz frontend/dist/ root@ham.codes:/tmp/frontend
+
+step "Backup old version..."
+#ssh root@ham.codes 'mv /data/doppelkopf /data/backup/doppelkopf_bak'
+
+step "Activate new version..."
+#ssh root@ham.codes 'mv /tmp/frontend/ /data/doppelkopf'
+
+step "Smoke test..."
+status=`curl --silent --head https://www.hamvocke.com | head -1 | cut -f 2 -d' '`
+
+if [ "$status" != "200" ]
+then
+    error "Failed: status was other than '200': was '$status'"
+    exit 1
+fi
+
+success "Done. All good."
