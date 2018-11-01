03fde93408d95a3c827828ad57dfe71030fde921
Ham Vocke <hermann.vocke@gmail.com>
2018-11-01 20:14:38 +0100

    add scorecard story

--- frontend/stories/index.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/vue";
-
-import Scorecard from "@/components/Scorecard";
-
-storiesOf("Scorecard", module).add("empty", () => ({
-  components: { Scorecard },
-  template: "<Scorecard :scorecard='{}' />"
-}));
---
+++ frontend/stories/scorecard.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from "@storybook/vue";
+
+import Scorecard from "@/components/Scorecard";
+import { Player } from "@/models/player";
+import { Scorecard as ScorecardModel } from "@/models/scorecard";
+
+const players = [
+  new Player("Player 1"),
+  new Player("Player 2"),
+  new Player("Player 3"),
+  new Player("Player 4")
+];
+const sc = new ScorecardModel(players);
+
+sc.addScore([players[0], players[2]], 3);
+sc.addScore([players[1], players[3]], 6);
+sc.addScore([players[0], players[2]], 2);
+sc.addScore([players[2], players[0]], 1);
+
+storiesOf("Scorecard", module)
+  .add("empty", () => ({
+    components: { Scorecard },
+    template: "<Scorecard :scorecard='{}' />"
+  }))
+  .add("with data", () => ({
+    components: { Scorecard },
+    template: "<Scorecard :scorecard='" + JSON.stringify(sc) + "' />"
+  }));
