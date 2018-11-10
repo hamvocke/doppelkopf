f501a3eecf4e03a49508db18cb9a5e3943ce5b11
Ham Vocke <hermann.vocke@gmail.com>
2018-11-10 12:57:06 +0100

    simplify welcome page

--- frontend/src/components/WelcomeScreen.vue
+++ frontend/src/components/WelcomeScreen.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="welcome" >
+  <div class="welcome">
+    <div class="logo">
+      🤖
+    </div>
     <h1>Doppelkopf</h1>
-
-    <h2>👋 Hey there!</h2>
-    <p>This is an online version of <a href="https://en.wikipedia.org/wiki/Doppelkopf">Doppelkopf</a>, a German card game. It's playable but still a work in progress. Feel free to mess around with the game as much as you like. Some of the basics will work. It won't be a lot of fun as you're playing against a shitty AI and a lot of elements of the game are missing.</p>
-
-    <p>If you want to take a look behind the scenes, you can find the source code on <a href="https://github.com/hamvocke/doppelkopf">GitHub</a> and more details on <a href="https://www.hamvocke.com/blog/doppelkopf/">my blog</a>.</p>
 
     <button class="button start-game" v-on:click='game.start()'>
       Start Game!
@@ -29,24 +27,15 @@ export default {
 @import "../assets/css/colors.css";
 
 .welcome {
-  padding: 60px;
+  padding-top: 10%;
+}
+
+.logo {
+  font-size: 8em;
 }
 
 .welcome h1 {
-  font-size: 8em;
+  font-size: 5em;
   color: var(--black);
-  text-transform: uppercase;
-  letter-spacing: -0.06em;
-}
-
-.welcome h2 {
-  font-size: 2em;
-  color: var(--black);
-}
-
-.welcome p {
-  width: 50%;
-  margin: 2em auto;
-  font-size: 1.2em;
 }
 </style>
