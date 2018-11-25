f90cf729e7c62752d51f78361beab147fdb7760d
Ham Vocke <hermann.vocke@gmail.com>
2018-11-25 17:01:36 +0100

    fix table alignment, make scorecard wider

--- frontend/src/components/Scorecard.vue
+++ frontend/src/components/Scorecard.vue
@@ -3,16 +3,16 @@
     <h1>Score</h1>
     <table>
       <tr>
-        <th class="player" v-for='player in players' :key='player.id'>
+        <th class="player number" v-for='player in players' :key='player.id'>
           {{ player.name }}
         </th>
-        <th>Punkte</th>
+        <th class="number">Punkte</th>
       </tr>
       <tr class="scoreLine" v-for='scoreLine in scorecard.scoreLines' :key='scoreLine.id'>
-        <td v-for='player in players' :key='player.id'>
+        <td v-for='player in players' :key='player.id' class="number">
           {{ scoreLine.score[player.id] }}
         </td>
-        <td>{{ scoreLine.points }}</td>
+        <td class="number">{{ scoreLine.points }}</td>
       </tr>
     </table>
 
@@ -56,19 +56,23 @@ export default {
   right: calc(-50vw + 50%);
   margin-left: auto;
   margin-right: auto;
-  width: 50%;
+  width: 66%;
 }
 
 .scorecard table {
+  text-align: left;
   width: 90%;
   padding: 6px;
   table-layout: fixed;
 }
 
-.scorecard th,
-.scorecard td {
-  line-height: 2em;
+th.number,
+td.number {
   text-align: right;
+}
+
+th, td {
+  line-height: 2em
 }
 
 .scorecard th {
