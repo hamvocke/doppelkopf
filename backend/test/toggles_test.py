5d859ae986bffdd3e348c7906a2d834e14e791d6
Hermann Vocke <hvocke@thoughtworks.com>
2019-02-27 16:55:12 +0100

    add and find features

--- backend/test/toggles_test.py
+++ backend/test/toggles_test.py
@@ -1,16 +1,24 @@
 from backend import toggles
 
 
-sample_toggle = toggles.Feature("sample feature", enabled=True)
+sample_feature = toggles.Feature("sample feature", enabled=True)
 
 
 def test_building_feature():
-    assert sample_toggle.enabled is True
+    assert sample_feature.enabled is True
 
 
 def test_switch_feature_state():
-    sample_toggle.toggle()
-    assert sample_toggle.enabled is False
+    sample_feature.toggle()
+    assert sample_feature.enabled is False
 
-    sample_toggle.toggle()
-    assert sample_toggle.enabled is True
+    sample_feature.toggle()
+    assert sample_feature.enabled is True
+
+
+def test_find_toggle():
+    toggles.features.add(sample_feature)
+
+    received_feature = toggles.features.find(name="sample feature")
+
+    assert received_feature == sample_feature
--- backend/toggles.py
+++ backend/toggles.py
@@ -1,3 +1,21 @@
+from dataclasses import dataclass
+
+
+class Features(object):
+    """docstring for Features."""
+
+    def __init__(self):
+        super(Features, self).__init__()
+        self.feature_list = {}
+
+    def add(self, feature):
+        self.feature_list.update({feature.name: feature})
+
+    def find(self, name=None):
+        return self.feature_list.get(name)
+
+
+@dataclass
 class Feature(object):
     """A Feature can be used to activate or deactivate functionality at
     runtime. It's status (enabled/disabled) can be queried to change the
@@ -12,10 +30,11 @@ class Feature(object):
             print("I'm doing b")
     """
 
-    def __init__(self, name, enabled):
-        super(Feature, self).__init__()
-        self.name = name
-        self.enabled = enabled
+    name: str
+    enabled: bool = False
 
     def toggle(self):
         self.enabled = not self.enabled
+
+
+features = Features()
