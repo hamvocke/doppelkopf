e824ee0febbb776cf57ed1121c1116b2d1f3bbe7
Ham Vocke <hermann.vocke@gmail.com>
2019-03-27 08:58:36 +0100

    add flash message component

---
+++ frontend/src/components/FlashMessage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flashMessage">
+    <div class="icon" v-if="icon">{{ icon }}</div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlashMessage",
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/css/colors.css";
+
+.flashMessage {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+</style>
---
+++ frontend/tests/unit/components/FlashMessage.test.js
@@ -0,0 +1,30 @@
+import FlashMessage from "@/components/FlashMessage";
+import { mount } from "@vue/test-utils";
+import VueTestUtils from "@vue/test-utils";
+
+VueTestUtils.config.mocks["$t"] = () => {};
+
+describe("FlashMessage.vue", () => {
+  test("should show message", () => {
+    const wrapper = mount(FlashMessage, {
+      propsData: { message: "some message" }
+    });
+    expect(wrapper.find(".flashMessage").text()).toBe("some message");
+  });
+
+  test("should show icon if present", () => {
+    const wrapper = mount(FlashMessage, {
+      propsData: { message: "some message", icon: ":)" }
+    });
+
+    expect(wrapper.find(".icon").text()).toBe(":)");
+  });
+
+  test("should not show icon if none is provided", () => {
+    const wrapper = mount(FlashMessage, {
+      propsData: { message: "some message" }
+    });
+
+    expect(wrapper.find(".icon").exists()).toBe(false);
+  });
+});
