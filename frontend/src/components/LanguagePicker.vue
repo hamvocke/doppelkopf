6b4f4fa0ad36fec27d23adabacc7f663673100f1
Ham Vocke <hermann.vocke@gmail.com>
2019-04-21 17:12:46 +0200

    add language picker component

---
+++ frontend/src/components/LanguagePicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="language">
+    {{ $t("language") }}:&nbsp;
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in availableLanguages" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LanguagePicker",
+  data: function() {
+    return {
+      availableLanguages: ["en", "de"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/css/app.css";
+
+.language {
+  margin: 6px;
+}
+</style>
---
+++ frontend/tests/unit/components/LanguagePicker.test.js
@@ -0,0 +1,16 @@
+import LanguagePicker from "@/components/LanguagePicker";
+import { mount } from "@vue/test-utils";
+import VueTestUtils from "@vue/test-utils";
+
+VueTestUtils.config.mocks["$t"] = (msg) => msg;
+VueTestUtils.config.mocks["$i18n"] = { locale: "en" };
+
+
+
+describe("LanguagePicker.vue", () => {
+  test("should show select with two language options", () => {
+    const wrapper = mount(LanguagePicker);
+    expect(wrapper.find(".language").text()).toContain("en");
+    expect(wrapper.find(".language").text()).toContain("de");
+  });
+});
