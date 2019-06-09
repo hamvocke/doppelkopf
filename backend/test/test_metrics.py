0f0e2debe56f7524eae7e9bca5854028b6433791
Ham Vocke <hermann.vocke@gmail.com>
2019-06-09 14:47:34 +0200

    add test for db, metrics setup command

--- backend/test/test_metrics.py
+++ backend/test/test_metrics.py
@@ -15,3 +15,17 @@ def test_send_metrics(mocked_influx):
     mocked_influx.return_value.write.assert_called_once_with(
         data="foo value=1", protocol="line"
     )
+
+
+def test_init_influx_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_create_database():
+        Recorder.called = True
+
+    monkeypatch.setattr('doppelkopf.metrics.create_database', fake_create_database)
+    result = runner.invoke(args=['init-influx'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
+
