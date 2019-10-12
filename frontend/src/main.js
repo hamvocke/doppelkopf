f3d9b26c14310bc2ec7e30162014dfe4f411eb28
Ham Vocke <hermann.vocke@gmail.com>
2019-10-12 14:09:33 +0200

    upgrade dependencies, set sentry environment correctly

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import * as Sentry from "@sentry/browser";
+import * as Integrations from '@sentry/integrations';
 
 import "@/assets/css/app.css";
 import i18n from "./i18n";
@@ -15,6 +16,6 @@ new Vue({
 
 Sentry.init({
   dsn: "https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644",
-  integrations: [new Sentry.Integrations.Vue({ Vue })],
-  environment: process.env.NODE_ENV
+  integrations: [new Integrations.Vue({ Vue })],
+  environment: process.env.NODE_ENV || process.env.server_env || "not-set"
 });
