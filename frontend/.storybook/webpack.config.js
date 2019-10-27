7ce15ae71b520cdc0d6253edf50937d807aea102
Ham Vocke <hermann.vocke@gmail.com>
2019-10-27 11:04:33 +0100

    fix storybook rendering issue

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = async ({ config, mode }) => {
   config.resolve.alias = {
     "@": path.resolve(__dirname, "../src"),
+    'vue$': 'vue/dist/vue.esm.js',
   }
   return config;
 };
