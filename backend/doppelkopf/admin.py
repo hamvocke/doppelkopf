9c114ae1ee30b91f409521e51b47d328ddb141db
Ham Vocke <hermann.vocke@gmail.com>
2019-10-31 18:35:48 +0100

    save toggle state

--- backend/doppelkopf/admin.py
+++ backend/doppelkopf/admin.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-from doppelkopf.toggles import Toggle
+from flask import Blueprint, render_template, request, redirect, url_for
+from .toggles import Toggle
+from .db import db
 
 blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -10,6 +11,19 @@ def index():
 
 
 @blueprint.route("/toggles", methods=["GET"])
-def toggles_endpoint():
+def toggles_list():
     all_toggles = Toggle.query.all()
     return render_template("admin/toggles.html", toggles=all_toggles)
+
+
+@blueprint.route("/toggles/submit", methods=["POST"])
+def toggles_submit():
+    all_toggles = Toggle.query.all()
+
+    for toggle in all_toggles:
+        # only enabled toggles end up in the request
+        toggle.enabled = True if request.form.get(str(toggle.id)) else False
+        db.session.add(toggle)
+
+    db.session.commit()
+    return redirect(url_for('admin.toggles_list'))
--- backend/test/test_admin.py
+++ backend/test/test_admin.py
@@ -3,8 +3,9 @@ from doppelkopf.db import db
 
 
 def test_should_have_index_endpoint(client):
-    response = client.get("/admin")
+    response = client.get("/admin/")
     assert response.status_code == 200
+
 
 def test_should_have_toggles_endpoint(client):
     response = client.get("/admin/toggles")
@@ -12,9 +13,25 @@ def test_should_have_toggles_endpoint(client):
 
 
 def test_should_render_toggles_from_db(client):
-    toggle = Toggle(name="some-toggle", enabled=True)
-    db.session.add(toggle)
-    db.session.commit()
-
+    save_toggle("some-toggle")
     response = client.get("/admin/toggles")
     assert b"some-toggle" in response.data
+
+
+def test_should_save_toggle_state(client):
+    one = save_toggle("some-toggle", True)
+    two = save_toggle("some-other-toggle", False)
+
+    client.post("/admin/toggles/submit", data={two.id: 'on'})
+
+    one = Toggle.query.get(one.id)
+    two = Toggle.query.get(two.id)
+    assert one.enabled is False
+    assert two.enabled is True
+
+
+def save_toggle(name="some-toggle", enabled=True) -> Toggle:
+    toggle = Toggle(name=name, enabled=enabled)
+    db.session.add(toggle)
+    db.session.commit()
+    return toggle
