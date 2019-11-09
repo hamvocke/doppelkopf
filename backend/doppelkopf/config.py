767c4a28c8bdb1036153ea8bf78404543f396681
Ham Vocke <hermann.vocke@gmail.com>
2019-11-09 12:06:40 +0100

    mount sqlite as docker volume

--- backend/doppelkopf/config.py
+++ backend/doppelkopf/config.py
@@ -27,12 +27,16 @@ class ProductionConfig(Config):
     INFLUXDB_HOST = os.environ.get("INFLUXDB_HOST", Config.INFLUXDB_HOST)
     INFLUXDB_USER = os.environ.get("INFLUXDB_USER", Config.INFLUXDB_USER)
     INFLUXDB_PASS = os.environ.get("INFLUXDB_PASS", Config.INFLUXDB_PASS)
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI", "sqlite:////app/db.sqlite")  # defined in docker-compose.yml
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DB_URI", "sqlite:////app/db/db.sqlite"
+    )  # defined in docker-compose.yml
 
 
 class DevelopmentConfig(Config):
     ENV_NAME = "dev"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database-dev.sqlite")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+        basedir, "database-dev.sqlite"
+    )
     DEBUG = True
 
 
