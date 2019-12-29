b1907f6fd07ae6114ccbe9cf2104fdafa76cd4d8
Ham Vocke <hermann.vocke@gmail.com>
2019-12-29 11:26:06 +0100

    add flash messages to notifier

--- frontend/src/models/notifier.js
+++ frontend/src/models/notifier.js
@@ -1,6 +1,9 @@
 import { uniqueId } from "lodash-es";
 let instance;
 
+const INFO_TYPE = 0;
+const FLASH_TYPE = 1;
+
 export class Notifier {
   constructor() {
     if (instance) {
@@ -13,7 +16,24 @@ export class Notifier {
   }
 
   info(message) {
-    this.messages.push({ id: uniqueId("message_"), text: message });
+    this.messages.push({
+      id: uniqueId("message_"),
+      text: message,
+      type: INFO_TYPE
+    });
+
+    window.setTimeout(() => {
+      this.messages.pop();
+    }, 4000);
+  }
+
+  flash(message) {
+    this.messages.push({
+      id: uniqueId("flash_"),
+      text: message,
+      type: FLASH_TYPE
+    });
+
     window.setTimeout(() => {
       this.messages.pop();
     }, 4000);
--- frontend/tests/unit/models/notifier.test.js
+++ frontend/tests/unit/models/notifier.test.js
@@ -4,6 +4,10 @@ const notifier = new Notifier();
 
 jest.useFakeTimers();
 
+beforeEach(() => {
+  jest.runAllTimers();
+});
+
 test("notifier is always the same instance", () => {
   const anotherNotifier = new Notifier();
   expect(notifier).toBe(anotherNotifier);
@@ -14,6 +18,15 @@ test("should add notification", () => {
 
   expect(notifier.messages).toHaveLength(1);
   expect(notifier.messages[0].text).toBe("Hello World");
+  expect(notifier.messages[0].type).toBe(0);
+});
+
+test("should add flash message", () => {
+  notifier.flash("Fuchs gefangen");
+
+  expect(notifier.messages).toHaveLength(1);
+  expect(notifier.messages[0].text).toBe("Fuchs gefangen");
+  expect(notifier.messages[0].type).toBe(1);
 });
 
 test("should remove notification after timeout", () => {
