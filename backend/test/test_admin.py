e262b0537c8003444d3bca0395012c1901b5a330
Ham Vocke <hvocke@stackoverflow.com>
2020-01-27 09:25:13 +0100

    fix admin tests after login

--- backend/test/test_admin.py
+++ backend/test/test_admin.py
@@ -3,20 +3,21 @@ from doppelkopf.toggles import Toggle
 from doppelkopf.db import db
 
 
-def test_should_have_index_endpoint(client):
+def test_admin_should_redirect_to_login(client):
     response = client.get("/admin/")
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 
-def test_should_have_toggles_endpoint(client):
+def test_toggles_should_redirect_to_login(client):
     response = client.get("/admin/toggles")
-    assert response.status_code == 200
+    assert response.status_code == 302
 
 
 def test_should_render_stats_on_index(client):
-    response = client.post("/api/game/new")
-
-    response = client.get("/admin/")
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        client.post("/api/game/new")
+        response = client.get("/admin/")
 
     assert response.status_code == 200
     assert b"Games started" in response.data
@@ -24,7 +25,9 @@ def test_should_render_stats_on_index(client):
 
 def test_should_render_toggles_from_db(client):
     save_toggle("some-toggle")
-    response = client.get("/admin/toggles")
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        response = client.get("/admin/toggles")
     assert b"some-toggle" in response.data
 
 
@@ -32,7 +35,9 @@ def test_should_save_toggle_state(client):
     one = save_toggle("one", True)
     two = save_toggle("two", False)
 
-    client.post("/admin/toggles/submit", data={two.id: "on"})
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        client.post("/admin/toggles/submit", data={two.id: "on"})
 
     one = Toggle.query.get(one.id)
     two = Toggle.query.get(two.id)
@@ -44,7 +49,9 @@ def test_should_update_last_changed_date_when_enabling(client):
     one = save_toggle("one", False)
     old_time = one.last_changed_at
 
-    client.post("/admin/toggles/submit", data={one.id: "on"})
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        client.post("/admin/toggles/submit", data={one.id: "on"})
 
     new_time = Toggle.query.get(one.id).last_changed_at
     assert new_time > old_time
@@ -54,7 +61,9 @@ def test_should_update_last_changed_date_when_disabling(client):
     one = save_toggle("one", True)
     old_time = one.last_changed_at
 
-    client.post("/admin/toggles/submit", data={})
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        client.post("/admin/toggles/submit", data={})
 
     new_time = Toggle.query.get(one.id).last_changed_at
     assert new_time > old_time
@@ -64,7 +73,9 @@ def test_should_not_update_last_changed_date_when_state_is_not_changed(client):
     one = save_toggle("one", True)
     old_time = one.last_changed_at
 
-    client.post("/admin/toggles/submit", data={one.id: "on"})
+    with client:
+        client.post("/admin/login", data={"email": "admin", "password": "totally-secret"})
+        client.post("/admin/toggles/submit", data={one.id: "on"})
 
     new_time = Toggle.query.get(one.id).last_changed_at
     assert new_time == old_time
