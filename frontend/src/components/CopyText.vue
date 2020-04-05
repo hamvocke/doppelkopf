af9d5022c9519772827d930b9142ed44b969b328
Ham Vocke <hermann.vocke@gmail.com>
2020-04-05 19:36:31 +0200

    show icon, text dynamically in copy text component

--- frontend/src/components/CopyText.vue
+++ frontend/src/components/CopyText.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="copyText">
     <input type="text" :value="text" readonly />
-    <button class="" @click="copyTextToClipboard()">
-      <span v-if="icon">{{ icon }}</span>
-      Copy
+    <button
+      class="button button-secondary button-plain"
+      @click="copyTextToClipboard()"
+    >
+      <Icon :name="icon" />
+      <span class="text">{{ buttonText }}</span>
     </button>
   </div>
 </template>
 
 <script>
+import Icon from "@/components/Icon";
+
 export default {
   name: "CopyText",
+  components: {
+    Icon
+  },
   props: {
     text: {
       type: String,
       required: true
-    },
-    icon: {
-      type: String,
-      required: false,
-      default: null
     }
+  },
+  data: function() {
+    return {
+      buttonText: "Copy",
+      icon: "clipboard"
+    };
   },
   methods: {
     copyTextToClipboard: function() {
       navigator.clipboard.writeText(this.text);
+      let oldText = this.buttonText;
+      this.buttonText = "Copied!";
+      this.icon = "check";
+      setTimeout(() => {
+        this.buttonText = oldText;
+        this.icon = "clipboard";
+      }, 1500);
     }
   }
 };
@@ -48,10 +64,15 @@ input {
 
 button {
   margin-left: -3px;
-  padding: 12px 16px;
-  border: none;
-  color: var(--white);
-  background-color: var(--lightblue);
   border-radius: 0 4px 4px 0;
+  font-size: 1em;
+  padding: 10px 16px 10px 8px;
+  background-color: var(--lightgray);
+  display: flex;
+  align-items: center;
+}
+
+.text {
+  padding-left: 6px;
 }
 </style>
--- frontend/src/components/Icon.vue
+++ frontend/src/components/Icon.vue
@@ -33,6 +33,39 @@
       <line x1="12" y1="17" x2="12.01" y2="17" />
     </svg>
   </span>
+  <span v-else-if="name === 'check'">
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="css-i6dzq1"
+    >
+      <polyline points="20 6 9 17 4 12"></polyline>
+    </svg>
+  </span>
+  <span v-else-if="name === 'clipboard'">
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      stroke-width="2"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="css-i6dzq1"
+    >
+      <path
+        d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
+      ></path>
+      <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+    </svg>
+  </span>
   <span v-else>
     <span>Invalid icon name</span>
   </span>
