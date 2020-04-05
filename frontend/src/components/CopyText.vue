2f18a1f12f47ca293e1af5586aad8bd7282f67d8
Ham Vocke <hermann.vocke@gmail.com>
2020-04-05 16:03:47 +0200

    add copy text component

---
+++ frontend/src/components/CopyText.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="copyText">
+    <input type="text" :value="text" readonly />
+    <button class="">
+      <span v-if="icon">{{ icon }}</span>
+      Copy
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CopyText",
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: null
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/css/vars.css";
+
+.copyText {
+  display: flex;
+  align-items: center;
+}
+
+input {
+  padding: 12px 2px;
+  font-size: 1.4em;
+  font-family: monospace;
+  border: none;
+  border-radius: 4px 0 0 4px;
+}
+
+button {
+  margin-left: -3px;
+  padding: 12px 16px;
+  border: none;
+  color: var(--white);
+  background-color: var(--lightblue);
+  border-radius: 0 4px 4px 0;
+}
+</style>
---
+++ frontend/src/stories/copyText.stories.js
@@ -0,0 +1,29 @@
+import VueI18n from "vue-i18n";
+
+import CopyText from "@/components/CopyText";
+
+import "@/assets/css/app.css";
+
+let locale = require("@/locales/de.json");
+let i18nOpts = {
+  locale: "de",
+  messages: {}
+};
+
+i18nOpts["messages"]["de"] = locale;
+
+export default {
+  title: "Copy Text"
+};
+
+export const icon = () => ({
+  components: { CopyText },
+  template: '<CopyText text="tunnel-snakes-rule" icon="🦊" />',
+  i18n: new VueI18n(i18nOpts)
+});
+
+export const no_icon = () => ({
+  components: { CopyText },
+  template: '<CopyText text="tunnel-snakes-rule" />',
+  i18n: new VueI18n(i18nOpts)
+});
