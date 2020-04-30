f9a570f5e93f6b4dba439daa16ffda3f0fcb7d89
Ham Vocke <hermann.vocke@gmail.com>
2020-04-30 22:04:06 +0200

    include frontend base url in config

--- frontend/src/models/config.js
+++ frontend/src/models/config.js
@@ -1,15 +1,18 @@
 const config = {
   test: {
+    baseUrl: "http://localhost:8080",
     backend_base_url: "http://localhost:5000",
     debug: true,
     testing: true
   },
   development: {
+    baseUrl: "http://localhost:8080",
     backend_base_url: "http://localhost:5000",
     debug: true,
     testing: false
   },
   production: {
+    baseUrl: "http://doppelkopf.ham.codes",
     backend_base_url: "https://doppelkopf.ham.codes",
     debug: false,
     testing: false
