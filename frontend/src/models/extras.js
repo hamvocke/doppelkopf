954a63323a5ac7fc90c244ba7325028a73a64e4a
Ham Vocke <hermann.vocke@gmail.com>
2020-06-13 14:04:27 +0200

    remove old code

--- frontend/src/models/extras.js
+++ frontend/src/models/extras.js
@@ -1,13 +1,3 @@
-export const DOPPELKOPF = "doppelkopf";
-export const FOX = "fox";
-export const WIN = "win";
-export const BEAT_RE = "beat_re";
-export const NO_90 = "no_90";
-export const NO_60 = "no_60";
-export const NO_30 = "no_30";
-export const NO_POINTS = "no_points";
-
-
 export const extras = {
   doppelkopf: "doppelkopf",
   fox: "fox",
@@ -17,4 +7,4 @@ export const extras = {
   no_60: "no_60",
   no_30: "no_30",
   no_points: "no_points",
-}
+};
