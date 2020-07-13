b6356e84a0297363190d4b8d1e52ae17dec8d26e
Ham Vocke <hermann.vocke@gmail.com>
2020-07-13 19:51:18 +0200

    use unified deployment step for new infrastructure

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -17,18 +17,18 @@ docker-compose push
 e_mute "Done"
 
 e_step "Start Docker containers"
-scp -o StrictHostKeyChecking=no docker-compose{.prod,}yml root@ham.codes:/data/doppelkopf/docker-compose.yml
+scp -o StrictHostKeyChecking=no docker-compose{.prod,}yml root@staging.ham.codes:/data/doppelkopf/
 ssh -T root@ham.codes -o StrictHostKeyChecking=no << EOF
     cd /data/doppelkopf
     docker-compose pull
-    docker-compose up -d
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 EOF
 e_mute "Done"
 
 e_step "Smoke tests..."
 sleep 5
-smoke_test https://doppelkopf.ham.codes
-smoke_test https://doppelkopf.ham.codes/api/
+smoke_test https://doppelkopf.staging.ham.codes
+smoke_test https://doppelkopf.staging.ham.codes/api/
 e_mute "Done"
 
 e_success "Done. All good."
