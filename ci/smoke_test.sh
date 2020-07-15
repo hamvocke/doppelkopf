266fb8ee94f9070333cbbca6bc3e17f3558b55a1
Ham Vocke <hermann.vocke@gmail.com>
2020-07-15 20:51:58 +0200

    retry smoke tests on failure

--- ci/smoke_test.sh
+++ ci/smoke_test.sh
@@ -7,11 +7,27 @@ function smoke_test {
 
   e_step "smoke testing ${url}"
 
-  status=`curl --silent --head $url | head -1 | cut -f 2 -d' '`
-  if [ "$status" != "200" ]
+  n=0
+  success=0
+  until [ "$n" -ge 5 ]
+  do
+    status=`curl --silent --head $url | head -1 | cut -f 2 -d' '`
+    if [ "$status" != "200" ]
+    then
+        e_error "Failed: status was other than '200': was '$status'"
+        e_step "Retrying..."
+    else
+      success=1
+      break
+    fi
+    n=$((n+1))
+    sleep 5
+  done
+
+  if [ "$success" != 1 ]
   then
-      e_error "Failed: status was other than '200': was '$status'"
-      exit 1
+    e_error "Smoke test failed"
+    exit 1
   fi
 
   e_success "Smoke test successful"
