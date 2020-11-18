98da06ba8f2f6426ad74eec1996df963f944dc53
Ham Vocke <ham@hamvocke.com>
2020-11-18 21:08:40 +0100

    show vue errors when debug is enabled

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import * as Sentry from "@sentry/browser";
 import * as Integrations from "@sentry/integrations";
-
 import "@/assets/css/app.css";
 import i18n from "./i18n";
 import router from "./router";
+import { Config } from "@/models/config";
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = Config.debug;
 
 new Vue({
   i18n,
@@ -18,6 +18,6 @@ new Vue({
 
 Sentry.init({
   dsn: "https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644",
-  integrations: [new Integrations.Vue({ Vue })],
+  integrations: [new Integrations.Vue({ Vue, logErrors: Config.debug })],
   environment: process.env.NODE_ENV || process.env.server_env || "not-set"
 });
