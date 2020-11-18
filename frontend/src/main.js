26d94093671319c26921a623561018a590e4e98a
Ham Vocke <ham@hamvocke.com>
2020-11-18 21:23:40 +0100

    enable sentry tracing, don't init sentry in tests

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import * as Sentry from "@sentry/browser";
-import * as Integrations from "@sentry/integrations";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
 import "@/assets/css/app.css";
 import i18n from "./i18n";
 import router from "./router";
@@ -16,8 +17,22 @@ new Vue({
   render: h => h(App)
 }).$mount("#app");
 
-Sentry.init({
-  dsn: "https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644",
-  integrations: [new Integrations.Vue({ Vue, logErrors: Config.debug })],
-  environment: process.env.NODE_ENV || process.env.server_env || "not-set"
-});
+if (!Config.testing) {
+  Sentry.init({
+    dsn: "https://69b3af20d4fa454f851a6be71502334c@sentry.io/1235644",
+    integrations: [
+      new Integrations.BrowserTracing(),
+      new VueIntegration({
+        Vue,
+        logErrors: Config.debug,
+        tracing: true,
+        tracingOptions: {
+          trackComponents: true,
+        },
+      })
+    ],
+    tracesSampleRate: Config.debug ? 1.0 : 0.2,
+    environment: process.env.NODE_ENV || process.env.server_env || "not-set"
+  });
+}
+
