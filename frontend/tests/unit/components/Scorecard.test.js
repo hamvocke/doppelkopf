d3dceb17a0a531b2c7fff8fa46048e095ae2dc42
Ham Vocke <ham@hamvocke.com>
2020-11-27 20:25:53 +0100

    stub count translation

--- frontend/tests/unit/components/Scorecard.test.js
+++ frontend/tests/unit/components/Scorecard.test.js
@@ -8,7 +8,7 @@ import { mount } from "@vue/test-utils";
 import VueTestUtils from "@vue/test-utils";
 
 VueTestUtils.config.mocks["$t"] = key => key;
-VueTestUtils.config.mocks["$tc"] = msg => msg;
+VueTestUtils.config.mocks["$tc"] = (msg, count) => `${count} ${msg}`;
 
 let players;
 let score;
