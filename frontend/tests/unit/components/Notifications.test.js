3aeb6c67e36a9039410d908c8ce1837bb506b2b3
Ham Vocke <ham@hamvocke.com>
2020-12-16 16:15:33 +0100

    add test for handling notification clicks
    skipped for now, seems to be a bug with how vue-test-utils handles
    transitions. Upgrading to v1 next to see if this fixes things (they say
    so)

--- frontend/tests/unit/components/Notifications.test.js
+++ frontend/tests/unit/components/Notifications.test.js
@@ -5,8 +5,13 @@ import VueTestUtils from "@vue/test-utils";
 
 VueTestUtils.config.mocks["$t"] = msg => msg;
 VueTestUtils.config.mocks["$tc"] = msg => msg;
+jest.useFakeTimers();
 
 describe("Notifications.vue", () => {
+  afterEach(() => {
+    jest.runAllTimers();
+  });
+
   it("should display message", () => {
     const wrapper = mount(Notifications);
 
@@ -28,9 +33,34 @@ describe("Notifications.vue", () => {
   it("should display sticky messages", () => {
     const wrapper = mount(Notifications);
 
-    new Notifier().sticky("An update is available!");
+    new Notifier().sticky("An update is available!", null, jest.fn());
 
     expect(wrapper.vm.stickies).toHaveLength(1);
     expect(wrapper.vm.stickies[0].text).toBe("An update is available!");
+  });
+
+  // TODO: re-enable after upgrading vue-test-utils to v1
+  it.skip("should handle sticky messages click", () => {
+    const wrapper = mount(Notifications);
+    const onClick = jest.fn();
+
+    new Notifier().sticky("An update is available!", null, onClick);
+
+    const allStickies = wrapper.findAll(".message .clickable");
+    expect(allStickies).toHaveLength(1);
+
+    allStickies.at(1).trigger("click");
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it.skip("should dismiss sticky message on dismiss click", () => {
+    const wrapper = mount(Notifications);
+
+    new Notifier().sticky("An update is available!", null, jest.fn());
+
+    const sticky = wrapper.find(".message .clickable");
+    sticky.trigger("click");
+    expect(wrapper.vm.stickies).toHaveLength(0);
   });
 });
