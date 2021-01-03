f5992f3b63e5674fd83a1a0a9665e14b1a0aac40
Ham Vocke <ham@hamvocke.com>
2021-01-03 12:34:35 +0100

    fix layout jumps

--- frontend/src/components/Trick.vue
+++ frontend/src/components/Trick.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="trick">
-    <div class="cards trick-card">
+    <div class="cards">
       <transition-group name="card" tag="span">
         <Card
-          v-for="playedCard in playerPlayedCards"
+          v-for="playedCard in cards"
           :key="playedCard.id"
           :card="playedCard.card"
           :player="playedCard.player"
@@ -43,37 +43,19 @@ export default {
 .trick {
   height: 100%;
   width: 100%;
+  display: flex;
+  align-items: center;
   justify-content: center;
-  align-items: stretch;
 }
 
 .cards > span {
   display: grid;
   grid-template-areas:
-    "left top right"
-    "left bottom right";
-  grid-template-columns: 1fr auto 1fr;
-  grid-template-rows: 1fr 1fr;
-  /* Remove gaps for overlapping effect */
-  column-gap: 6px;
-  row-gap: 64px;
-  height: 100%;
-}
-
-.trick-card {
-  height: 100%;
-}
-/*
-following problems so far:
-card class is only added to the trick when card is there, hence the layout is always in movement, due to flex moving correspondingly
-- card containers have to be static, with min-height and min-width vals, matching the card-size.
-either we create a static grid with empty cells checking 4x4 direction === card or we create "dummy" cards that don't show but still create the correct layout  
-
-*/
-
-.cards .card {
-  /* Remove to have overlapping effect*/
-  margin: 6px;
+    "top top top"
+    "left empty right"
+    "bottom bottom bottom";
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
 }
 
 .card-enter-active {
