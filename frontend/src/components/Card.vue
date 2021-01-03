eb211f2f797301d4f3153248f0c204f07e9a88b3
Ham Vocke <ham@hamvocke.com>
2021-01-03 19:42:35 +0100

    explicitly set card-select transition properties

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -97,6 +97,7 @@ export default {
   text-align: center;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.25s cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-property: top, box-shadow;
   user-select: none;
   font-weight: 900;
 }
