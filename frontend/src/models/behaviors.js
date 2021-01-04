06954923d77ba7563aa7d457f01648c0aef375c6
David Kaufmann <dave-kaufmann@web.de>
2021-01-04 21:38:07 +0100

    added 'basic' fehlrule

--- frontend/src/models/behaviors.js
+++ frontend/src/models/behaviors.js
@@ -26,3 +26,34 @@ export class RandomCardBehavior {
     return null;
   }
 }
+
+export class BasicRuleBasedBehaviour {
+  cardToPlay(hand, baseCard) {
+    if (!baseCard.isTrump()){
+      return this.fehlRule(hand, baseCard);
+    }
+    return sample(playableCards(hand.cards, baseCard));
+  }
+
+  /*
+    needs to know if own card is higher than highest in trick.
+    Therefore we need to know the current trick
+  */
+  fehlRule(hand, baseCard){
+    fehlCards = hand.cards.filter(card => card.suits === basecard.suits)
+    if (fehlCards.length > 0){
+      highestFehl = fehlcards[0]
+      if (fehlCards.length > 0) {
+        if (baseCard.value() < highestFehl.value() && highestFehl.value() === 11){
+          return highestFehl;
+        }else{
+          return fehlcards.reverse()[0]
+        }
+      }
+    }else{
+      // play trump. usually use high-value cards
+      trumpCards = hand.cards.filter(card => card.isTrump() === true)
+      return sample(trumpCards)
+    }
+  }
+}
