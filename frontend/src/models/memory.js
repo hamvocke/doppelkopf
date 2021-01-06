addea91080f4b7c3388cbbf7782997360f69d2f1
David Kaufmann <dave-kaufmann@web.de>
2021-01-06 21:53:28 +0100

    added memory class that will take care of the AI player memory

---
+++ frontend/src/models/memory.js
@@ -0,0 +1,48 @@
+import { uniqueId } from "lodash-es";
+
+/**
+ * I would love some kind of interface here.
+ * Interface.js seems to be a reasonable possibility.
+ * Right now we'll stick to this.
+ */
+
+export class PercentageMemory {
+  constructor(percent) {
+    this.percent = percent;
+    this.playedCards = [];
+    this.id = uniqueId("memory_percent");
+  }
+
+  memorize(playedCard) {
+    if (Math.random() <= this.percent) this.playedCards.push(playedCard);
+  }
+}
+
+export class PerfectMemory {
+  constructor() {
+    this.playedCards = [];
+    this.id = uniqueId("memory_perfect");
+  }
+
+  memorize(playedCard) {
+    this.playedCards.push(playedCard);
+  }
+}
+
+export class PriorityMemory {
+  constructor() {
+    this.playedCards = [];
+    this.id = uniqueId("memory_priority");
+  }
+
+  memorize(playedCard) {
+    /*
+    Superb function to take care of 'important' cards
+    Will memorize all by value
+    Queens, Tens, Aces
+    */
+    if ([3, 10, 11].includes(playedCard.card.value)) {
+      this.playedCards.push(playedCard);
+    }
+  }
+}
