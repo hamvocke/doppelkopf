7b37b49a2fda8609ca1630b6bbdb4e6643bae211
Ham Vocke <ham@hamvocke.com>
2021-02-22 20:52:06 +0100

    move scorecard to ts

--- frontend/src/models/game.ts
+++ frontend/src/models/game.ts
@@ -6,13 +6,14 @@ import { Scorecard } from "@/models/scorecard";
 import { RingQueue } from "@/models/ringQueue";
 import { generateNames } from "@/models/random";
 import { Telemetry } from "@/models/telemetry";
+import { Round } from "./round";
 
 export class Game {
   players: Player[];
   private playerOpeningOrder: RingQueue<Player>;
   deck: Deck;
   scorecard: any;
-  currentRound: any;
+  currentRound: Round;
 
   constructor(players: Player[] = []) {
     this.players = players;
