76d7225fc6cf631a7a09c0c8ff04935d6e4f05e4
Ham Vocke <ham@hamvocke.com>
2021-02-25 20:49:38 +0100

    migrate announcementsbutton component to ts

--- frontend/src/models/announcements.ts
+++ frontend/src/models/announcements.ts
@@ -1,8 +1,8 @@
 export enum Announcement {
-  Re = "Re",
-  Kontra = "Kontra",
-  No90 = "No 90",
-  No60 = "No 60",
-  No30 = "No 30",
-  NoPoints = "No points"
+  Re = "re", // note: the enum values are used as i18n keys so don't change them without changing i18n keys, too
+  Kontra = "kontra",
+  No90 = "no-90",
+  No60 = "no_60",
+  No30 = "no_30",
+  NoPoints = "no_points"
 }
