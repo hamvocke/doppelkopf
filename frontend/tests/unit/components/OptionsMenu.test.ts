23f2acd5c8ac6cfb62338331242faf09c00b7e1e
Ham Vocke <ham@hamvocke.com>
2021-02-28 19:58:57 +0100

    convert options menu to ts

---
+++ frontend/tests/unit/components/OptionsMenu.test.ts
@@ -0,0 +1,41 @@
+import OptionsMenu from "@/components/OptionsMenu.vue";
+import { mount, config } from "@vue/test-utils";
+
+config.mocks["$t"] = (msg: string) => msg;
+config.mocks["$tc"] = (msg: string) => msg;
+config.mocks["$i18n"] = { locale: "en" };
+
+describe("OptionsMenu.vue", () => {
+  test("should show options icon", async () => {
+    const wrapper = mount(OptionsMenu);
+
+    await wrapper.setData({ visible: true });
+
+    expect(wrapper.find(".options-icon").exists()).toBe(true);
+  });
+
+  test("should not show options", async () => {
+    const wrapper = mount(OptionsMenu);
+
+    await wrapper.setData({ visible: false });
+
+    expect(wrapper.find(".options-menu").exists()).toBe(false);
+  });
+
+  test("should show options", async () => {
+    const wrapper = mount(OptionsMenu);
+
+    await wrapper.setData({ visible: false });
+    await wrapper.find(".options-icon").trigger("click");
+
+    expect(wrapper.find(".options-menu").exists()).toBe(true);
+  });
+
+  test("should contain language picker", async () => {
+    const wrapper = mount(OptionsMenu);
+
+    await wrapper.setData({ visible: true });
+
+    expect(wrapper.find(".options-menu #languages").exists()).toBe(true);
+  });
+});
