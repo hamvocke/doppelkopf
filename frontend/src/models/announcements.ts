2a8a9784653fb5edf5efbe670345446100c83c62
Ham Vocke <ham@hamvocke.com>
2021-03-01 21:21:27 +0100

    fix 'no 90' i18n key

--- frontend/src/models/announcements.ts
+++ frontend/src/models/announcements.ts
@@ -1,7 +1,7 @@
 export enum Announcement {
   Re = "re", // note: the enum values are used as i18n keys so don't change them without changing i18n keys, too
   Kontra = "kontra",
-  No90 = "no-90",
+  No90 = "no_90",
   No60 = "no_60",
   No30 = "no_30",
   NoPoints = "no_points"
