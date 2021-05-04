260c8c7b8e94c1a645ac13174c8733ebac33f344
Ham Vocke <hermann.vocke@gmail.com>
2021-05-04 21:48:31 +0200

    test loading message in waiting room

--- frontend/tests/unit/views/WaitingRoom.test.ts
+++ frontend/tests/unit/views/WaitingRoom.test.ts
@@ -1,13 +1,30 @@
 import WaitingRoom from "@/views/WaitingRoom.vue";
-import { Config } from "@/models/config";
-import { mount, config } from "@vue/test-utils";
+import { config, shallowMount } from "@vue/test-utils";
 
 config.mocks["$t"] = (msg: string) => msg;
 config.mocks["$tc"] = (msg: string) => msg;
 config.mocks["$i18n"] = { locale: "en" };
 
 describe("WaitingRoom.vue", () => {
+  test("should render loading state", () => {
+    const multiplayerHandlerMock = {
+      fetchRoom: jest.fn().mockRejectedValue(new Error("something failed"))
+    };
+
+    const wrapper = shallowMount(WaitingRoom, {
+      propsData: { gameName: "1" },
+      data: () => ({
+        isLoading: true,
+        error: undefined,
+        multiplayerHandler: multiplayerHandlerMock
+      })
+    });
+
+    expect(wrapper.find(".loading").exists()).toBe(true);
+    expect(wrapper.find(".error").exists()).toBe(false);
+    expect(wrapper.find(".wrapper").exists()).toBe(false);
+  });
+
   test.todo("should render waiting players");
-  test.todo("should render loading state");
   test.todo("should render loading errors");
 });
