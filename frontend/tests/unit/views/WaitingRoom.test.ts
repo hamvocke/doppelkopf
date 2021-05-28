4548f6bd2635b3bb29a8765e773b6bece90a9682
Ham Vocke <hermann.vocke@gmail.com>
2021-05-28 21:11:21 +0200

    add waiting room component test

--- frontend/tests/unit/views/WaitingRoom.test.ts
+++ frontend/tests/unit/views/WaitingRoom.test.ts
@@ -1,3 +1,5 @@
+import { CreateResponse } from "@/helpers/multiplayerHandler";
+import { Player } from "@/models/player";
 import WaitingRoom from "@/views/WaitingRoom.vue";
 import { config, shallowMount } from "@vue/test-utils";
 
@@ -45,5 +47,40 @@ describe("WaitingRoom.vue", () => {
     expect(wrapper.find(".loading").exists()).toBe(false);
     expect(wrapper.find(".error").exists()).toBe(true);
     expect(wrapper.find(".wrapper").exists()).toBe(false);
+  });
+
+  test("should render players", async () => {
+    const mockedResponse: CreateResponse = {
+      game: {
+        id: "some-game",
+        players: [{ name: "some player" }]
+      }
+    };
+
+    const multiplayerHandlerMock = {
+      fetchRoom: jest.fn().mockResolvedValue(mockedResponse)
+    };
+
+    const wrapper = shallowMount(WaitingRoom, {
+      propsData: { gameName: "1" },
+      data: () => ({ multiplayerHandler: multiplayerHandlerMock }),
+      created: jest.fn()
+    });
+
+    await wrapper.setData({
+      isLoading: false,
+      error: undefined,
+      players: [new Player("some player", true)]
+    });
+
+    expect(wrapper.find(".loading").exists()).toBe(false);
+    expect(wrapper.find(".error").exists()).toBe(false);
+    expect(wrapper.find(".wrapper").exists()).toBe(true);
+    expect(
+      wrapper
+        .findAll(".players .player")
+        .at(0)
+        .text()
+    ).toEqual("some player");
   });
 });
