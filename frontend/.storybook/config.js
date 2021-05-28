aab0d60dc07bff97493c2e4dea33a0ee7470551d
Ham Vocke <hermann.vocke@gmail.com>
2021-05-28 21:26:28 +0200

    remove storybook.js

--- frontend/.storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/vue';
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-
-Vue.use(VueI18n);
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
