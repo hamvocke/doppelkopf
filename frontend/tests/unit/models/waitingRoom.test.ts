a27d459fa355dc37d058221db612fb3145e90ce6
Ham Vocke <hermann.vocke@gmail.com>
2021-06-23 20:54:23 +0200

    test waitingRoom.join

--- frontend/tests/unit/models/waitingRoom.test.ts
+++ frontend/tests/unit/models/waitingRoom.test.ts
@@ -1,12 +1,32 @@
+import { WebsocketClient, Event } from "@/helpers/websocketClient";
 import { Player } from "@/models/player";
 import { WaitingRoom } from "@/models/waitingRoom";
+import { mocked } from "ts-jest/utils";
+
+jest.mock("@/helpers/websocketClient");
+let websocketMock = mocked(WebsocketClient);
 
 let waitingRoom: WaitingRoom;
 
+let player1 = new Player("player 1");
+let player2 = new Player("player 2");
+let player3 = new Player("player 3");
+let player4 = new Player("player 4");
+
 beforeEach(() => {
+  websocketMock.mockReset();
   waitingRoom = new WaitingRoom(42);
 });
 
 test("should generate game URL", () => {
   expect(waitingRoom.gameUrl).toEqual("http://localhost:8080/#/wait/42");
+});
+
+test("should send join event", () => {
+  waitingRoom.join(player1);
+
+  expect(websocketMock.mock.instances[0].emit).toHaveBeenCalledWith(
+    Event.join,
+    expect.anything()
+  );
 });
