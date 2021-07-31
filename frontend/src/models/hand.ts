60dbb85bf22b9848e65fba8e23a8443a0d71d678
David Kaufmann <mail@davidkaufmann.de>
2021-07-31 12:47:45 +0200

    add new check if hand is playable
    will check is hand has more than 3 trumps.
    reasoning is that 3 trumps usually isn't playable and will result in a poverty.
    since this isn't implemented yet, we just skip this possibility.

--- frontend/src/models/hand.ts
+++ frontend/src/models/hand.ts
@@ -54,7 +54,9 @@ export class Hand {
     return (
       this.cards.filter(card => card.rank === Rank.King).length < 5 &&
       this.cards.filter(card => card.value >= 10).length < 7 &&
-      this.highest().compareTo(jack.of(Suit.Diamonds)) < 0
+      this.highest().compareTo(jack.of(Suit.Diamonds)) < 0 &&
+      // ToDo remove last line as soon as poverty is playable
+      this.trumps().length > 3
     );
   }
 
