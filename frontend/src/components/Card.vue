d3f6be4159ba0a41a9c3b865f90c5ceacbf921e2
Ham Vocke <hermann.vocke@gmail.com>
2021-11-21 14:25:07 +0100

    randomize card rotation, make cards overlap in trick

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -91,7 +91,7 @@ export default class Card extends Vue {
   padding: 6px;
   border-radius: 10px;
   text-align: center;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.42), 0 1px 2px rgba(0, 0, 0, 0.34);
   transition: all 0.25s cubic-bezier(0.25, 0.8, 0.25, 1);
   transition-property: top, box-shadow;
   user-select: none;
@@ -188,7 +188,8 @@ export default class Card extends Vue {
   }
 
   .card-bottom {
-    display: none;
+    right: 3px;
+    bottom: 3px;
   }
 
   .card-top .rank {
