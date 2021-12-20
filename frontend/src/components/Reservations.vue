82233c29d5c48f48ee86bd6d462179da13074bf4
Ham Vocke <hermann.vocke@gmail.com>
2021-12-20 15:23:41 +0100

    add reservations component

---
+++ frontend/src/components/Reservations.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="reservations">Reservations</div>
+</template>
+
+<script>
+export default {
+  name: "Reservations",
+  props: {
+    game: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+#root {
+  height: 100%;
+}
+</style>
