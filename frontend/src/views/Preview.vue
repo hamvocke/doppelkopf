9eb663300349dd93fbe74f855eaa27918f6d3b7f
Ham Vocke <hermann.vocke@gmail.com>
2021-12-20 15:22:50 +0100

    add new preview view

---
+++ frontend/src/views/Preview.vue
@@ -0,0 +1,33 @@
+<!-- This view is intended for previewing in-development components only. -->
+
+<template>
+  <div id="root">
+    <reservations :game="game" />
+  </div>
+</template>
+
+<script>
+import { Game } from "@/models/game";
+import Reservations from "@/components/Reservations.vue";
+
+export default {
+  name: "Preview",
+  components: {
+    Reservations
+  },
+  props: {
+    game: {
+      type: Object,
+      default: function() {
+        return Game.singlePlayer();
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+#root {
+  height: 100%;
+}
+</style>
