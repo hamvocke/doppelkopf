1510f2e90e09da306c071b0df08b9328043b9e4f
Ham Vocke <hermann.vocke@gmail.com>
2021-12-21 16:17:17 +0100

    clean up Table.test.ts

--- frontend/tests/unit/components/Table.test.ts
+++ frontend/tests/unit/components/Table.test.ts
@@ -16,32 +16,22 @@ beforeEach(() => {
 });
 
 describe("Table.vue", () => {
-  test("should render players", () => {
+  test("should render table components", () => {
     const wrapper = mount(Table, { propsData: { game: game } });
     expect(wrapper.findAll("div.player")).toHaveLength(4);
-  });
-
-  test("should render trick", () => {
-    const wrapper = mount(Table, { propsData: { game: game } });
     expect(wrapper.find("div.trick").exists()).toBe(true);
-  });
-
-  test("should render controls", () => {
-    const wrapper = mount(Table, { propsData: { game: game } });
     expect(wrapper.find("div.controls").exists()).toBe(true);
   });
 
   test("should hide Scorecard if game is not finished", () => {
-    const mockGame = Game.singlePlayer();
-    mockGame.currentRound.isFinished = () => false;
+    game.currentRound.isFinished = () => false;
 
-    const wrapper = mount(Table, { propsData: { game: mockGame } });
+    const wrapper = mount(Table, { propsData: { game: game } });
 
     expect(wrapper.find("div.scorecard").exists()).toBe(false);
   });
 
   test("should show Scorecard if game is finished", () => {
-    const mockGame = Game.singlePlayer();
     game.players[0].points = () => 120;
     game.players[1].points = () => 120;
     const reParty = new Party(PartyName.Re, game.players[0], game.players[3]);
@@ -52,10 +42,10 @@ describe("Table.vue", () => {
     );
     const stubScore = new Score(reParty, kontraParty);
     stubScore.winner = () => reParty;
-    mockGame.currentRound.score = stubScore;
-    mockGame.currentRound.isFinished = () => true;
+    game.currentRound.score = stubScore;
+    game.currentRound.isFinished = () => true;
 
-    const wrapper = mount(Table, { propsData: { game: mockGame } });
+    const wrapper = mount(Table, { propsData: { game: game } });
 
     expect(wrapper.find("div.scorecard").exists()).toBe(true);
   });
