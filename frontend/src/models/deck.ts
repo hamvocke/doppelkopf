bb9b1649f6022a7cb227ea0c7525fae7d37fa930
David Kaufmann <mail@davidkaufmann.de>
2021-12-28 12:57:34 +0100

    deck creates cards with trump- and cardorders according to rules

--- frontend/src/models/deck.ts
+++ frontend/src/models/deck.ts
@@ -1,10 +1,140 @@
-import { Card, Rank, Suit } from "@/models/card";
+import {
+  ace,
+  Card,
+  compareSuites,
+  defaultCardOrder,
+  defaultTrumps,
+  king,
+  Rank,
+  Suit,
+  ten
+} from "@/models/card";
 import { shuffle } from "@/models/random";
+import { RoundRules } from "@/models/rules";
+
 export class Deck {
   cards: Card[];
+  rules: RoundRules[] | undefined;
 
-  constructor() {
-    this.cards = shuffle(new Array<Card>().concat(...allCards));
+  constructor(rules?: RoundRules[]) {
+    this.cards = shuffle(new Array<Card>().concat(...defaultCardOrder));
+    // TODO replace with card.rank != NINE if SHARP DOKO
+    // TODO sharp doko should come from game settings, not round
+    if (rules?.includes(RoundRules.SHARP_DOKO)) {
+      this.cards.filter(card => card.value != 0);
+    }
+    this.refreshRules(rules);
+  }
+
+  refreshRules(rules?: RoundRules[]) {
+    this.cards.forEach(card => {
+      card.trumps = defaultTrumps!;
+      card.cardOrder = defaultCardOrder;
+      if (rules?.includes(RoundRules.JACK_SOLO)) {
+        card.trumps = this.buildRankSoloTrumps(Rank.Jack);
+        card.cardOrder = [...this.cards].sort(this.compareRankSolo(Rank.Jack));
+      }
+      if (rules?.includes(RoundRules.QUEEN_SOLO)) {
+        card.trumps = this.buildRankSoloTrumps(Rank.Queen);
+        card.cardOrder = [...this.cards].sort(this.compareRankSolo(Rank.Queen));
+      }
+      if (rules?.includes(RoundRules.HEARTS_SOLO)) {
+        card.trumps = this.buildSuitSoloTrumps(Suit.Hearts);
+        card.cardOrder = [
+          ...this.buildSuitDoubleSoloTrumps(Suit.Hearts),
+          ...this.buildNonTrumpOrder(Suit.Hearts)
+        ];
+      }
+      if (rules?.includes(RoundRules.SPADES_SOLO)) {
+        card.trumps = this.buildSuitSoloTrumps(Suit.Spades);
+        card.cardOrder = [
+          ...this.buildSuitDoubleSoloTrumps(Suit.Spades),
+          ...this.buildNonTrumpOrder(Suit.Spades)
+        ];
+      }
+      if (rules?.includes(RoundRules.CLUBS_SOLO)) {
+        card.trumps = this.buildSuitSoloTrumps(Suit.Clubs);
+        card.cardOrder = [
+          ...this.buildSuitDoubleSoloTrumps(Suit.Clubs),
+          ...this.buildNonTrumpOrder(Suit.Clubs)
+        ];
+      }
+      if (rules?.includes(RoundRules.PIGGIES)) {
+        this.moveToFront(ace.of(Suit.Diamonds).second());
+        this.moveToFront(ace.of(Suit.Diamonds).first());
+      }
+      if (rules?.includes(RoundRules.NO_TRUMPS)) {
+        card.trumps = [];
+        card.cardOrder = [...this.cards].sort(this.compareRankSolo());
+      }
+    });
+  }
+
+  private compareRankSolo = (rank?: Rank) => (
+    oneCard: Card,
+    anotherCard: Card
+  ) => {
+    if (oneCard.rank === rank && anotherCard.rank !== rank) {
+      return -1;
+    }
+    if (oneCard.rank !== rank && anotherCard.rank === rank) {
+      return 1;
+    }
+    if (oneCard.rank === rank && oneCard.rank === anotherCard.rank) {
+      return compareSuites(oneCard.suit, anotherCard.suit);
+    }
+    if (oneCard.rank !== rank && anotherCard.rank !== rank) {
+      if (oneCard.suit === anotherCard.suit) {
+        return anotherCard.value - oneCard.value;
+      }
+    }
+    return compareSuites(oneCard.suit, anotherCard.suit);
+  };
+
+  private buildNonTrumpOrder(except: Suit) {
+    return [Suit.Clubs, Suit.Spades, Suit.Hearts, Suit.Diamonds]
+      .filter(suit => suit !== except)
+      .map(suit => [
+        ace.of(suit),
+        ...(suit === Suit.Hearts ? [] : [ten.of(suit)]),
+        king.of(suit)
+      ])
+      .flat()
+      .map(c => [c.first(), c.second()])
+      .flat();
+  }
+
+  private buildRankSoloTrumps(rank: Rank) {
+    return [...this.cards]
+      .filter(card => card.rank === rank && card.id === 0)
+      .sort((a, b) => {
+        return compareSuites(b.suit, a.suit);
+      })
+      .reverse();
+  }
+
+  private buildSuitSoloTrumps(suit: Suit) {
+    return [
+      ...defaultTrumps.slice(0, -4),
+      ace.of(suit).first(),
+      ...(suit === Suit.Hearts ? [] : [ten.of(suit).first()]),
+      king.of(suit).first()
+    ];
+  }
+
+  private buildSuitDoubleSoloTrumps(suit: Suit) {
+    return this.buildSuitSoloTrumps(suit)
+      .map(c => [c.first(), c.second()])
+      .flat();
+  }
+
+  private moveToFront(card: Card) {
+    const first = card.cardOrder.findIndex(c => c.equals(card));
+    const spliced = card.cardOrder.splice(first, 1)[0];
+    card.cardOrder.splice(0, 0, spliced);
+    const index = card.trumps.findIndex(c => c.is(card));
+    const cut = card.trumps.splice(index, 1)[0];
+    card.trumps.splice(0, 0, cut);
   }
 }
 
--- frontend/tests/unit/models/deck.test.ts
+++ frontend/tests/unit/models/deck.test.ts
@@ -1,5 +1,6 @@
 import { Deck } from "@/models/deck";
-import { Card, Rank, Suit } from "@/models/card";
+import { ace, Card, Rank, Suit } from "@/models/card";
+import { RoundRules } from "@/models/rules";
 
 test("deck has 40 cards", () => {
   const deck = new Deck();
@@ -20,4 +21,60 @@ test("deck is shuffled", () => {
   const anotherDeck = new Deck();
 
   expect(oneDeck.cards).not.toEqual(anotherDeck.cards);
+});
+
+test("deck updates piggies to be highest trump", () => {
+  const deck = new Deck([RoundRules.PIGGIES]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(24);
+  expect(deck.cards[0].trumps[0].is(ace.of(Suit.Diamonds))).toBe(true);
+  expect(deck.cards[0].cardOrder[0].is(ace.of(Suit.Diamonds))).toBe(true);
+  expect(deck.cards[0].cardOrder[1].is(ace.of(Suit.Diamonds))).toBe(true);
+});
+
+test("deck deals jack solo", () => {
+  const deck = new Deck([RoundRules.JACK_SOLO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(8);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.rank === Rank.Jack)
+  ).toHaveLength(8);
+});
+
+test("deck deals queen solo", () => {
+  const deck = new Deck([RoundRules.QUEEN_SOLO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(8);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.rank === Rank.Queen)
+  ).toHaveLength(8);
+});
+
+test("deck deals diamonds solo", () => {
+  const deck = new Deck([RoundRules.DIAMONDS_SOLO, RoundRules.SHARP_DOKO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(24);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.suit === Suit.Diamonds)
+  ).toHaveLength(10);
+});
+
+test("deck deals hearts solo", () => {
+  const deck = new Deck([RoundRules.HEARTS_SOLO, RoundRules.SHARP_DOKO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(22);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.suit === Suit.Hearts)
+  ).toHaveLength(10);
+});
+
+test("deck deals spades solo", () => {
+  const deck = new Deck([RoundRules.SPADES_SOLO, RoundRules.SHARP_DOKO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(24);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.suit === Suit.Spades)
+  ).toHaveLength(10);
+});
+
+test("deck deals clubs solo", () => {
+  const deck = new Deck([RoundRules.CLUBS_SOLO, RoundRules.SHARP_DOKO]);
+  expect(deck.cards.filter(card => card.isTrump())).toHaveLength(24);
+  expect(
+    deck.cards.filter(card => card.isTrump() && card.suit === Suit.Clubs)
+  ).toHaveLength(10);
 });
