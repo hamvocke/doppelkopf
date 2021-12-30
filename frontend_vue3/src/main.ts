9d52e255fbc193094d8db0546b3a7ce3a4e89798
Ham Vocke <hermann.vocke@gmail.com>
2021-12-30 14:29:13 +0100

    setup i18n

--- frontend_vue3/src/main.ts
+++ frontend_vue3/src/main.ts
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from "vue-i18n";
+
+async function loadLocaleMessages() {
+  return {
+    de: (await import("./locales/de.json")).default,
+    en: (await import("./locales/en.json")).default,
+  };
+}
+
+const i18n = createI18n({
+  locale: navigator.language.split("-")[0] || "en",
+  fallbackLocale: "en",
+  messages: await loadLocaleMessages()
+});
+
+
+const app = createApp(App);
+app.use(i18n);
+app.mount("#app");
 
 createApp(App).mount('#app')
