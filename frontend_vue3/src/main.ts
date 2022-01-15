5193c5dbb98ed0670d966f1a375f09464cf55f43
Ham Vocke <hermann.vocke@gmail.com>
2022-01-15 11:08:08 +0100

    setup sentry, load feature toggles

--- frontend_vue3/src/main.ts
+++ frontend_vue3/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import { createI18n } from "vue-i18n";
+import * as Sentry from "@sentry/vue";
 import App from './App.vue'
 import router  from "./router";
 import "@/assets/css/app.css";
+import { Config } from "@/models/config";
+import { Features } from "@/models/features";
 
 // TODO: register service worker
-// TODO: set up sentry
 // TODO: set up eslint, prettier
 
 async function loadLocaleMessages() {
@@ -22,7 +24,18 @@ const i18n = createI18n({
 });
 
 
-createApp(App)
-  .use(i18n)
+let app = createApp(App)
+
+if (!Config.debug) {
+  Sentry.init({
+    app,
+    dsn: "https://69b3af20d4fa454f851a6be71502334c@o57417.ingest.sentry.io/1235644",
+  });
+}
+
+app.use(i18n)
   .use(router)
   .mount("#app");
+
+// load feature toggles
+Features.fetch();
