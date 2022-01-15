7cf23041792b5a502ff83fffa3054098408d3c94
Ham Vocke <hermann.vocke@gmail.com>
2022-01-15 11:52:56 +0100

    tweak eslint config

--- frontend_vue3/.eslintrc.js
+++ frontend_vue3/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
   env: {
     node: true,
-    'vue/setup-compiler-macros': true
+    "vue/setup-compiler-macros": true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    '@vue/typescript/recommended',
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
   ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
   rules: {
     // Enable vue/script-setup-uses-vars rule
-    'vue/script-setup-uses-vars': 'error',
+    "vue/script-setup-uses-vars": "error",
   }
 }
