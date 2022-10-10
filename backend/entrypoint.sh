f1d429fb7795825e76e350156e65dea3d134a818
Ham Vocke <hermann.vocke@gmail.com>
2022-10-10 18:45:06 +0200

    remove backend directory

--- backend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env sh
-
-pipenv run flask db upgrade
-pipenv run flask seed-data
-exec pipenv run python wsgi.py
--- backend/wsgi.py
+++
@@ -1,8 +0,0 @@
-from doppelkopf import create_app
-from doppelkopf.sockets import socketio
-
-
-app = create_app()
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
