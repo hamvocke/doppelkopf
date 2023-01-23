52f92dc184a4348ac839747e61484cc29fcce879
Ham Vocke <hermann.vocke@gmail.com>
2023-01-23 21:22:54 +0100

    add toggle component

---
+++ src/components/Toggle.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="toggle" :class="{ disabled }" @click="toggle">
+    <label>
+      {{ labelText }}
+      <input :checked="checked" type="checkbox" @change="toggle" />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+  },
+  labelText: {
+    type: String,
+    required: true,
+  },
+  checked: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:checked"]);
+
+function toggle() {
+  if (props.disabled) {
+    return;
+  }
+
+  emit("update:checked", !props.checked);
+}
+</script>
+
+<style scoped>
+@import "../assets/css/app.css";
+</style>
