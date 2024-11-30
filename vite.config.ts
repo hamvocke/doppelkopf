53c8f9a84ed819afc1a8b0f47f17128830da7d3b
Ham Vocke <ham@hamvocke.com>
2024-11-30 16:06:00 +0100

    update a shitload of dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  test: {
-    environment: "jsdom"
-  }
-});
+})
