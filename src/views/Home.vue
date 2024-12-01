41b90f4af0b31f94887024a02560680f625eda0d
Ham Vocke <ham@hamvocke.com>
2024-12-01 11:38:11 +0100

    tweak home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,32 @@
 <template>
   <div id="home">
-    <Notifications />
+    <OptionsMenu />
     <div class="welcome">
       <Logo />
 
       <h1>Doppelkopf</h1>
 
-      <div class="name-form">
-        <div class="form-wrapper">
-          <label for="player-name">
-            {{ $t("enter_name_label") }}
-          </label>
-          <input
-            id="player-name"
-            v-model="playerName"
-            class="input"
-            :placeholder="$t('enter_name_input')"
-            @blur="saveName()"
-          />
-        </div>
-      </div>
+      <p class="lead">{{ $t("home-page-teaser-lead") }}</p>
+      <p>{{ $t("home-page-teaser-text") }}</p>
+    </div>
+    <div class="name-form">
+      <label for="player-name">
+        {{ $t("enter_name_label") }}
+      </label>
+      <input id="player-name" v-model="playerName" class="input" :placeholder="$t('enter_name_input')"
+        @blur="saveName()" />
+    </div>
+    <div class="buttons">
+      <router-link to="/play">
+        <button class="button start-game">{{ $t("start-game") }}</button>
+      </router-link>
 
-      <div class="buttons">
-        <router-link to="/play" class="button start-game" tag="button">
-          {{ $t("start-game") }}
-        </router-link>
-
-        <router-link
-          v-if="showTutorial"
-          to="/learn"
-          class="button button-secondary tutorial"
-          tag="button"
-        >
-          {{ $t("how-to-play") }}
-        </router-link>
-        <a
-          v-else
-          :href="$t('tutorial-link')"
-          target="_blank"
-          class="button button-secondary tutorial-link"
-        >
-          {{ $t("how-to-play") }}
-        </a>
-      </div>
+      <router-link v-if="showTutorial" to="/learn">
+        <button class="button button-secondary tutorial">{{ $t("how-to-play") }}</button>
+      </router-link>
+      <a v-else :href="$t('tutorial-link')" target="_blank" class="button button-secondary tutorial-link">
+        {{ $t("how-to-play") }}
+      </a>
     </div>
   </div>
 </template>
@@ -51,7 +35,7 @@
 import { ref } from "vue";
 import { Features } from "@/models/features";
 import Logo from "@/components/Logo.vue";
-import Notifications from "@/components/Notifications.vue";
+import OptionsMenu from "@/components/OptionsMenu.vue";
 
 const playerName = ref((localStorage.name as string) || "");
 const showTutorial = ref(Features.enableTutorial);
@@ -68,17 +52,11 @@ function saveName() {
 
 #home {
   height: 100%;
-}
-
-.welcome {
+  overflow-x: auto;
   display: grid;
-  place-content: center;
-  height: 100%;
-}
-
-.logo {
-  font-size: 5em;
-  display: block;
+  align-content: center;
+  width: min(100%, 90ch);
+  margin: auto;
 }
 
 .welcome h1 {
@@ -88,8 +66,21 @@ function saveName() {
     3px 3px 0 var(--black), 4px 4px 0 var(--black), 5px 5px 0 var(--black),
     6px 6px 0 var(--red), 7px 7px 0 var(--red);
   font-size: clamp(48px, 10vw, 96px);
-  margin: 12px 0 64px;
+  margin: 1rem 0 3rem;
   text-align: center;
+}
+
+.welcome {
+  margin-bottom: 2rem;
+}
+
+.welcome p {
+  padding: 0 2rem;
+}
+
+.welcome .lead {
+  font-size: 1.2em;
+  font-style: italic;
 }
 
 .input {
@@ -104,25 +95,17 @@ function saveName() {
   outline: 3px solid var(--lightblue);
 }
 
-.name-form {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 3em;
+label {
+  margin-bottom: 0.4rem;
+  display: block;
 }
 
-.form-wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+.name-form {
+  margin: 0 auto 1.5rem auto;
 }
 
 .buttons {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-label {
-  margin-bottom: 0.6em;
+  margin: auto;
+  margin-bottom: 3rem;
 }
 </style>
