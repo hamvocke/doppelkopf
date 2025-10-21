11f41bcb3b3e2004780aacc117d923d99bb5c1f0
Ham Vocke <ham@hamvocke.com>
2025-10-21 10:07:40 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs_22 ];
+        };
+      });
+    };
+}
